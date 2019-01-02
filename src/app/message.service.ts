b7c8b5cb81a066908cc5ebccefa6b2781d8fb517
= <=>
2019-01-03 01:47:25 +0700

    comment code

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -3,18 +3,18 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-//service để thực hiện lấy dữ liệu trả về dưới dạng thông báo để hiện thị vào messagesComponent
+// service để thực hiện lấy dữ liệu trả về dưới dạng thông báo để hiện thị vào messagesComponent
 export class MessageService {
 
-  //property messages kiểu khởi tạo là mảng string rỗng
+  // property messages kiểu khởi tạo là mảng string rỗng
   messages: string[] = [];
   add(message: string) {
-    //mỗi lần gọi hàm add thì 1 message sẽ được thêm vào mảng
+    / /mỗi lần gọi hàm add thì 1 message sẽ được thêm vào mảng
     this.messages.push(message);
   }
-  //hàm clear thông báo
+  // hàm clear thông báo
   clear() {
     this.messages = [];
   }
 }
-//việc cần làm giờ là inject vào component thôi
+// việc cần làm giờ là inject vào component thôi
