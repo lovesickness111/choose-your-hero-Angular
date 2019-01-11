26bfe63fe2c6824639971a22d5cb2d576265d6b2
= <=>
2019-01-12 00:11:00 +0700

    attribute directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-tour-of-heroes';
   content = 'this is a string interpolation';
+
   isHide = true;
+  cone = true;
+  ctwo = true;
+
+  style = 'normal';
+  size = `24px`;
   toggleText() {
     this.isHide = !this.isHide;
+  }
+
+  toggleStyle() {
+    this.cone = !this.cone;
+    this.ctwo = !this.ctwo;
+    this.style = 'italic';
   }
 }
